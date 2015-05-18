350507581f2388320fdfb4f29b56e52da3e39367
William Davis <will@rbcplatform.com>
2015-05-18 16:10:10 -0700

    improved tests

--- spec/lib/bodhi_slam_spec.rb
+++ spec/lib/bodhi_slam_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe BodhiSlam do
   describe ".context" do
     context "with valid params" do
-      it "should yield a BodhiContext" do
+      it "should yield a Bodhi::Context" do
         BodhiSlam.context({ server: "test", namespace: "test" }) do |context|
           expect(context).to be_a Bodhi::Context
           expect(context).to be_valid
@@ -14,6 +14,13 @@ describe BodhiSlam do
     context "with invalid params" do
       it "should raise a Bodhi::Errors" do
         expect{ BodhiSlam.context({ server: nil, namespace: nil }){|context|} }.to raise_error(Bodhi::Errors)
+        
+        begin
+          BodhiSlam.context({ server: nil, namespace: nil }){|context|}
+        rescue Exception => e
+          expect(e.messages[:server]).to match_array(["must be present", "must be a string"])
+          expect(e.messages[:namespace]).to match_array(["must be present", "must be a string"])
+        end
       end
     end
   end
