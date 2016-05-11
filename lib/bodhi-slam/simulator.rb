bcd2579f7c989c0d6314036fbb42eb5906bfce33
willdavis <bdavis84@pacbell.net>
2016-05-10 17:11:49 -0700

    yield Bodhi::SimulationFrame now

--- lib/bodhi-slam/simulator.rb
+++ lib/bodhi-slam/simulator.rb
@@ -1,4 +1,12 @@
 module Bodhi
+  class SimulationFrame
+    include Bodhi::Properties
+    include Bodhi::Validations
+
+    property :iteration, type: Integer, default: 0
+    property :time, type: DateTime
+  end
+
   class Simulator
     include Bodhi::Properties
     include Bodhi::Validations
@@ -6,16 +14,18 @@ module Bodhi
     # Initial conditions
     property :starts_at, type: DateTime
     property :iterations, type: Integer
-    property :time_scale, type: String
+    property :time_units, type: String
+    property :time_scale, type: Integer, default: 1
 
     # Dynamic attributes
-    property :current_iteration, type: Integer, default: 0
-    property :current_time, type: DateTime
+    # Updated every iteration
+    property :current_frame, type: SimulationFrame
 
     # Model validations
     validates :starts_at, required: true
     validates :iterations, required: true, min: 1
-    validates :time_scale, required: true
+    validates :time_units, required: true
+    validates :time_scale, min: 1
 
     # ================
     # Class Methods
@@ -23,9 +33,9 @@ module Bodhi
 
     # Run a new simulation using the given +settings+ and +&block+
     # Yields the +simulation.current_time+ to the user defined +&block+
-    # Ignores +current_iteration+ and +current_time+ settings and zero's them before the simulation
+    # Ignores +current_frame+ settings and zero's them before the simulation
     #
-    #   Bodhi::Simulator.execute(starts_at: XXXXX, iterations: 10, time_scale: "days") do |current_time|
+    #   Bodhi::Simulator.execute(starts_at: "2016-05-10", iterations: 10, time_units: "days") do |current_time|
     #     # do some simulation stuff!
     #   end
     #
@@ -44,11 +54,15 @@ module Bodhi
         raise ArgumentError.new("Invalid settings: #{simulation.errors.to_a}")
       end
 
-      simulation.current_iteration = 0
-      simulation.current_time = simulation.starts_at
+      if simulation.current_frame.nil?
+        simulation.current_frame = Bodhi::SimulationFrame.new(iteration: 0, time: simulation.starts_at)
+      else
+        simulation.current_frame.iteration = 0
+        simulation.current_frame.time = simulation.starts_at
+      end
 
       until simulation.complete?
-        yield simulation.current_time
+        yield simulation.current_frame.clone
         simulation.increment
       end
     end
@@ -59,21 +73,20 @@ module Bodhi
 
     # returns true if the simulation has processed all iterations
     def complete?
-      current_iteration == iterations
+      self.current_frame.iteration == self.iterations
     end
 
     # Increments the simulation loop by one iteration
     # Updates +current_time+ to the new time offset
     # Updates +current_iteration+ to the next iteration
-    # Iteration count is zero based!
     #
-    #   simulation = Bodhi::Simulator.new(starts_at: "2016-05-13", iterations: 2, time_scale: "days")
+    #   simulation = Bodhi::Simulator.new(starts_at: "2016-05-13", iterations: 2, time_units: "days")
     #   simulation.increment
-    #   simulation #=> #<Bodhi::Simulator @starts_at: "2016-05-13" @iterations: 2, @time_scale: "days", @current_time: "2016-05-14", @current_iteration: 1 >
+    #   simulation #=> #<Bodhi::Simulator @starts_at: "2016-05-13" @iterations: 2, @time_units: "days", @current_time: "2016-05-14", @current_iteration: 1 >
     #
     def increment
-      self.current_iteration += 1
-      self.current_time = self.starts_at + (self.current_iteration * 1.send(self.time_scale))
+      self.current_frame.iteration += 1
+      self.current_frame.time = self.starts_at + (self.current_frame.iteration * 1.send(self.time_units) * self.time_scale)
     end
   end
 end
--- spec/lib/bodhi-slam/simulator_spec.rb
+++ spec/lib/bodhi-slam/simulator_spec.rb
@@ -1,51 +1,67 @@
 require 'spec_helper'
 
 describe Bodhi::Simulator do
-  let(:simulation){ Bodhi::Simulator.new(starts_at: "2016-05-10", iterations: 2, time_scale: "days") }
+  let(:simulation){ 
+    Bodhi::Simulator.new(
+      starts_at: "2016-05-10", 
+      iterations: 2,
+      time_units: "days",
+      time_scale: 1,
+      current_frame: { iteration: 0, time: Time.parse("2016-05-10") }
+    )
+  }
 
-  describe ".complete?" do
-    it "returns true if simulation is complete" do
-      simulation.iterations = 1
-      simulation.current_iteration = 1
-      expect(simulation.complete?).to be true
+  describe "Instance Methods" do
+    describe ".complete?" do
+      it "returns true if simulation is complete" do
+        simulation.iterations = 1
+        simulation.current_frame.iteration = 1
+
+        expect(simulation.complete?).to be true
+      end
+
+      it "returns false if simulation is not complete" do
+        simulation.iterations = 10
+        simulation.current_frame.iteration = 1
+
+        expect(simulation.complete?).to be false
+      end
     end
 
-    it "returns false if simulation is not complete" do
-      simulation.iterations = 10
-      simulation.current_iteration = 1
-      expect(simulation.complete?).to be false
+    describe ".increment" do
+      it "increments the :current_frame.iteration by 1" do
+        simulation.current_frame.iteration = 1
+        simulation.increment
+
+        expect(simulation.current_frame.iteration).to eq 2
+      end
+
+      it "increments the :current_frame.time by 1 :time_unit" do
+        simulation.current_frame.iteration = 0
+        simulation.current_frame.time = Time.parse("2016-05-13")
+        simulation.starts_at = Time.parse("2016-05-13")
+        simulation.time_units = "days"
+        simulation.time_scale = 1
+        simulation.increment
+
+        expect(simulation.current_frame.time).to eq Time.parse("2016-05-14")
+      end
     end
   end
 
-  describe ".increment" do
-    it "increments the :current_iteration by 1" do
-      simulation.current_iteration = 1
-      simulation.increment
+  describe "Class Methods" do
+    describe "#execute(settings, &block)" do
+      it "raises ArgumentError if the settings are not valid" do
+        expect { Bodhi::Simulator.execute(iterations: -1) }.to raise_error(ArgumentError, "Invalid settings: [\"starts_at is required\", \"iterations must be greater than or equal to 1\", \"time_units is required\"]")
+      end
 
-      expect(simulation.current_iteration).to eq 2
-    end
+      it "yields the :current_frame object to the &block" do
+        expect { |block| Bodhi::Simulator.execute(starts_at: "2016-05-10", iterations: 1, time_units: "days", time_scale: 1, &block) }.to yield_successive_args(Bodhi::SimulationFrame)
+      end
 
-    it "increments the :current_time by 1 :time_scale unit" do
-      simulation.current_iteration = 0
-      simulation.starts_at = Time.parse("2016-05-13")
-      simulation.current_time = Time.parse("2016-05-13")
-      simulation.time_scale = "days"
-      simulation.increment
-      expect(simulation.current_time).to eq Time.parse("2016-05-14")
-    end
-  end
-
-  describe "#execute(settings, &block)" do
-    it "raises ArgumentError if the settings are not valid" do
-      expect { Bodhi::Simulator.execute(iterations: -1) }.to raise_error(ArgumentError, "Invalid settings: [\"starts_at is required\", \"iterations must be greater than or equal to 1\", \"time_scale is required\"]")
-    end
-
-    it "yields the :current_time to the &block" do
-      expect { |block| Bodhi::Simulator.execute(starts_at: "2016-05-10", iterations: 1, time_scale: "days", &block) }.to yield_successive_args(Time.parse("2016-05-10"))
-    end
-
-    it "yields the :current_time as many times as the :iterations is set to" do
-      expect { |block| Bodhi::Simulator.execute(starts_at: "2016-05-10", iterations: 100, time_scale: "days", &block) }.to yield_control.exactly(100).times
+      it "yields the :current_frame object for each iteration" do
+        expect { |block| Bodhi::Simulator.execute(starts_at: "2016-05-10", iterations: 100, time_units: "days", time_scale: 1, &block) }.to yield_control.exactly(100).times
+      end
     end
   end
 end
